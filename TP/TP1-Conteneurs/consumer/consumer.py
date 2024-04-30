a3947bb997954b03bd6d3f70d874f7b66cf88700
ihab abadi <ihab@utopios.net>
2024-04-30 19:43:05 +0200

    le TP avec les volumes

--- TP/TP1-Conteneurs/consumer/consumer.py
+++ TP/TP1-Conteneurs/consumer/consumer.py
@@ -8,6 +8,8 @@ def show_temperature():
     # Envoyer une requête GET au producteur pour obtenir la température
     response = requests.get('http://producer:5000/temperature')
     temperature = response.json()['temperature']
+    with open('/app/data/temperatures.txt', 'a') as file:
+        file.write(f"{temperature}\n")
     return f"Current Temperature: {temperature}"
 
 if __name__ == '__main__':
--- TP/TP1-Conteneurs/setup-containers.sh
+++ TP/TP1-Conteneurs/setup-containers.sh
@@ -5,11 +5,12 @@ consumer_image="consumer_image"
 
 ## Réseau de type bridge
 docker network create temp-network
+docker volume create data-temperatures
 
 docker build -t $producer_image ./producer
 docker build -t $consumer_image ./consumer
 
 docker run --name producer --network temp-network -d $producer_image
-docker run --name consumer --network temp-network -p 5001:5001 -d $consumer_image
+docker run --name consumer --network temp-network -p 5001:5001 -v data-temperatures:/app/data -d $consumer_image
 
 
