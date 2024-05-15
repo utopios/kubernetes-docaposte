40c0fac2ae96a1f050515819f327613144ec995c
ihab abadi <ihab@utopios.net>
2024-05-15 20:27:13 +0200

    correction TP 2

--- kubernetes/TP-Vote-K8S/script.sh
+++ kubernetes/TP-Vote-K8S/script.sh
@@ -1,8 +1,15 @@
 ## Pour le dev, on utilisera un cluster kind (k8s sur docker)
 
 ## Etape 1 => création du cluster avec une configuration
-
+kind create cluster --name tp-kubernetes --config ./sources/kind/kind-config.yml
 ## Etape 2 => Construire les images et vérifier que les images sont disponibles sur le cluster.
+docker build -t result-image ./result/.
+docker build -t vote-image ./vote/.
+docker build -t worker-image ./worker/.
+
+kind load docker-image result-image --name tp-kubernetes
+kind load docker-image vote-image --name tp-kubernetes
+kind load docker-image worker-image --name tp-kubernetes
 
 ## Etape 3 => Ressources k8s
 
@@ -17,3 +24,18 @@
 ##### Nodeport ou loadBalancer ou un Ingress controller pour le routage à partir de l'extérieur vers result, vote
 ### Etape 3.3 => ressources de networkpolicies
 ### Deux networkpolicies pour le controle du traffic vers redis et postgres
+
+kubectl create -f sources/k8s/namespace.yml
+kubectl create -f sources/k8s/configmap.yml
+kubectl create -f sources/k8s/secrets.yml
+kubectl create -f sources/k8s/services/clusterip-postgres.yml
+kubectl create -f sources/k8s/services/clusterip-redis.yml
+kubectl create -f sources/k8s/services/nodeport-vote.yml
+kubectl create -f sources/k8s/services/nodeport-result.yml
+kubectl create -f sources/k8s/deployments/postgres-deployment.yml
+kubectl create -f sources/k8s/deployments/redis-deployment.yml
+kubectl create -f sources/k8s/deployments/vote-deployment.yml
+kubectl create -f sources/k8s/deployments/worker-deployment.yml
+kubectl create -f sources/k8s/deployments/result-deployment.yml
+kubectl create -f sources/k8s/networkpolicies/policy-postgres.yml
+kubectl create -f sources/k8s/networkpolicies/policy-redis.yml
