a80138e015a03fd2b64eb305eb76aa08acbb50ef
ihab abadi <ihab@utopios.net>
2024-05-21 18:49:30 +0200

    avec correction TP1

--- kubernetes/TP-Vote-K8S/script.sh
+++ kubernetes/TP-Vote-K8S/script.sh
@@ -1,7 +1,7 @@
 ## Pour le dev, on utilisera un cluster kind (k8s sur docker)
 
 ## Etape 1 => création du cluster avec une configuration
-kind create cluster --name tp-kubernetes --config ./sources/kind/kind-config.yml
+kind create cluster --name tp-kubernetes --config ./kind/kind-config.yml
 ## Etape 2 => Construire les images et vérifier que les images sont disponibles sur le cluster.
 docker build -t result-image ./result/.
 docker build -t vote-image ./vote/.
@@ -25,18 +25,18 @@ kind load docker-image worker-image --name tp-kubernetes
 ### Etape 3.3 => ressources de networkpolicies
 ### Deux networkpolicies pour le controle du traffic vers redis et postgres
 
-kubectl create -f sources/k8s/namespace.yml
-kubectl create -f sources/k8s/configmap.yml
-kubectl create -f sources/k8s/secrets.yml
-kubectl create -f sources/k8s/services/clusterip-postgres.yml
-kubectl create -f sources/k8s/services/clusterip-redis.yml
-kubectl create -f sources/k8s/services/nodeport-vote.yml
-kubectl create -f sources/k8s/services/nodeport-result.yml
-kubectl create -f sources/k8s/deployments/postgres-deployment.yml
-kubectl create -f sources/k8s/deployments/redis-deployment.yml
-kubectl create -f sources/k8s/deployments/vote-deployment.yml
-kubectl create -f sources/k8s/deployments/worker-deployment.yml
-kubectl create -f sources/k8s/deployments/result-deployment.yml
-kubectl create -f sources/k8s/networkpolicies/policy-postgres.yml
-kubectl create -f sources/k8s/networkpolicies/policy-redis.yml
+kubectl create -f k8s/namespace.yml
+kubectl create -f k8s/configmap.yml
+kubectl create -f k8s/secrets.yml
+kubectl create -f k8s/services/clusterip-postgres.yml
+kubectl create -f k8s/services/clusterip-redis.yml
+kubectl create -f k8s/services/nodeport-vote.yml
+kubectl create -f k8s/services/nodeport-result.yml
+kubectl create -f k8s/deployments/postgres-deployment.yml
+kubectl create -f k8s/deployments/redis-deployment.yml
+kubectl create -f k8s/deployments/vote-deployment.yml
+kubectl create -f k8s/deployments/worker-deployment.yml
+kubectl create -f k8s/deployments/result-deployment.yml
+kubectl create -f k8s/networkpolicies/policy-postgres.yml
+kubectl create -f k8s/networkpolicies/policy-redis.yml
 
