53bd0cb8e30f5d9444d02a359f15a683a8ac4364
ihab abadi <ihab@utopios.net>
2024-05-22 20:26:01 +0200

    utilisation des classes de stockages

--- kubernetes/demo-volumes/exercice1/script-delete.sh
+++ kubernetes/demo-volumes/exercice1/script-delete.sh
@@ -1,6 +1,7 @@
 # Création du namespace 
+kubectl delete -f mysql-deployment.yml
 
-kubectl delete -f namespace.yml
 kubectl delete -f pv.yml
 kubectl delete -f pvc.yml
-kubectl delete -f mysql-deployment.yml
+
+kubectl delete -f namespace.yml
